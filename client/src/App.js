61dc763dd8e73c56fc157b8f039ed5b0e6b5c005
david caguazango <felipe0160@gmail.com>
2021-11-18 08:00:36 -0500

    Card component finished

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
-import Navbar from "./components/Navbar";
-import Card from "./components/Card";
+import Navbar from "./components/Navbar/Navbar";
+import Card from "./components/Card/Card";
 import "./App.css";
 
 function App() {
--- client/src/components/Card/Card.js
+++ client/src/components/Card/Card.js
@@ -1,21 +1,22 @@
 import React from "react";
 import "./Card.css";
-import clock from "../clock.svg";
-import icon from "../img1.svg";
+import clock from "../../clock.svg";
+import icon from "../../img1.svg";
+import rectangle from "../../rectangle.png";
 
 function Card() {
   return (
     <div className="Card">
       <div className="FrameUp">
-        <div className="FrameImage">
-          <img src={icon} />
-        </div>
+        <img className="FrameImage" src={rectangle} alt="" />
+        <img className="Image" src={icon} alt="" />
         <p>Solcitudes de creacion de empresas</p>
       </div>
       <div className="FrameDown">
         <hr />
         <p>
-          <img src={clock} /> 2 solicitudes sin tratar{" "}
+          <img src={clock} alt="" />
+          <span> 2 solicitudes sin tratar</span>
         </p>
       </div>
     </div>
