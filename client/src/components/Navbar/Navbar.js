ed6f94adb96251675762d5fc2a2fe7a3ece31560
david caguazango <felipe0160@gmail.com>
2021-11-24 12:11:49 -0500

    Desktop version of cards ready

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -2,15 +2,50 @@ import React from "react";
 import "./Navbar.css";
 import logo from "../../logo.svg";
 import nav_menu from "../../nav_menu.svg";
+import Avatar from "../../navbarImages/Avatar.svg";
+import Admin from "../../navbarImages/Admin.svg";
+import Cv from "../../navbarImages/Cv.svg";
+import Empleo from "../../navbarImages/Empleo.svg";
+import Flecha from "../../navbarImages/Flecha.svg";
+import Lupa from "../../navbarImages/Lupa.svg";
 
 function Navbar() {
   return (
     <div className="Navbar">
-      <p>
-        <img className="Nav_logo" src={logo} alt="" />
-        <span>Gente PreValente</span>
-      </p>
-      <img className="Nav_image" src={nav_menu} alt="" />
+      <div className="Container">
+        <p className="Mobile">
+          <img className="Nav_logo" src={logo} alt="" />
+          <span>Gente PreValente</span>
+        </p>
+        <p className="Desktop">
+          <img src={Lupa} alt="" />
+          <span>Buscar...</span>
+        </p>
+      </div>
+      <div className="Container">
+        <p className="Desktop">
+          <img src={Admin} alt="" />
+          <span className="Admin">
+            Administración
+            <div className="Notifications">2</div>
+          </span>
+        </p>
+        <p className="Desktop">
+          <img src={Empleo} alt="" />
+          <span>Empleo</span>
+          <img src={Flecha} alt="" />
+        </p>
+        <p className="Desktop">
+          <img src={Cv} alt="" />
+          <span>Mi CV</span>
+        </p>
+        <p className="Desktop">
+          <img src={Avatar} alt="" />
+          <span>Daniel</span>
+          <img src={Flecha} alt="" />
+        </p>
+        <img className="Nav_image" src={nav_menu} alt="" />
+      </div>
     </div>
   );
 }
